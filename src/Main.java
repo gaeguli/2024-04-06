d124b1e6f9c572bd360507fc4c85551943b83215
gaeguli <moojin0830@gmail.com>
2024-04-07 01:01:27 +0900

    first

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,59 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        long first_num;
+        long second_num;
+        String a;
+        Scanner scanner = new Scanner(System.in);
+        boolean b = false;
+        while (!b){
+            try{
+                System.out.println("첫번째 숫자를 입력해주세요.");
+                first_num = scanner.nextLong();
+
+                System.out.println("두번째 숫자를 입력해주세요.");
+                second_num = scanner.nextLong();
+
+                System.out.println("연산자를 입력해주세요.");
+                a = scanner.next();
+                switch (a){
+                    case "+":
+                        System.out.println(first_num + second_num);
+                        b = true;
+                        break;
+
+                    case "-":
+                        System.out.println(first_num - second_num);
+                        b = true;
+                        break;
+
+                    case "*":
+                        System.out.println(first_num * second_num);
+                        b = true;
+                        break;
+
+                    case "/":
+                        if (second_num == 0) {
+                            System.out.println("0으로 나눌 수 없습니다.\n");
+                    }
+                    else{
+
+                        System.out.println(first_num / second_num);
+                        b = true;
+                    }
+                        break;
+
+                    default:
+                        System.out.println("올바른 문자를 입력해주세요.");
+                }
+
+            } catch (Exception e) {
+                System.out.println("올바른 문자를 입력해주세요\n");
+                scanner.nextLine();
+
+            }
+        }
+
     }
 }
